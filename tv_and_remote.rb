bbb11be5cbe62d0a73fcf0ad00d33ef55064106a
heydanhey <danhey@gmail.com>
2016-01-24 16:53:01 -0600

    Class Corrections

--- tv_and_remote.rb
+++ tv_and_remote.rb
@@ -22,50 +22,51 @@ end
 
 class Remote
   attr_accessor :tv
-  def initialize (tv=Tv.new)
+  def initialize (tv)
     @tv = tv
   end
 
   def toggle_power
-    tv.power = !tv.power
+    @tv.power = !tv.power
   end
 
   def increment_volume
-    tv.volume += 1
+    @tv.volume += 1
   end
 
   def decrement_volume
-    tv.volume -= 1
+    @tv.volume -= 1
   end
 
   def set_channel(channel)
-    tv.channel = channel
+    @tv.channel = channel
   end
 end
 
-#Create TV
-remote = Remote.new
+#Create TV/Remote
+tv = Tv.new
+remote = Remote.new(tv)
 
 puts "TESTING TV:"
-puts "Is power on?: #{remote.tv.power}"
-puts "Volume: #{remote.tv.volume}"
-puts "Channel: #{remote.tv.channel}"
+puts "Is power on?: #{tv.power}"
+puts "Volume: #{tv.volume}"
+puts "Channel: #{tv.channel}"
 puts "---------------"
 
 puts "TESTING TOGGLE POWER:"
 remote.toggle_power
-puts "Is power on?: #{remote.tv.power}"
-puts "Volume: #{remote.tv.volume}"
-puts "Channel: #{remote.tv.channel}"
+puts "Is power on?: #{tv.power}"
+puts "Volume: #{tv.volume}"
+puts "Channel: #{tv.channel}"
 puts "---------------"
 
 puts "TESTING VOLUME INCREMENT (x3):"
 remote.increment_volume
 remote.increment_volume
 remote.increment_volume
-puts "Is power on?: #{remote.tv.power}"
-puts "Volume: #{remote.tv.volume}"
-puts "Channel: #{remote.tv.channel}"
+puts "Is power on?: #{tv.power}"
+puts "Volume: #{tv.volume}"
+puts "Channel: #{tv.channel}"
 puts "---------------"
 
 puts "TESTING VOLUME DECREMENT (x4):"
@@ -73,16 +74,16 @@ remote.decrement_volume
 remote.decrement_volume
 remote.decrement_volume
 remote.decrement_volume
-puts "Is power on?: #{remote.tv.power}"
-puts "Volume: #{remote.tv.volume}"
-puts "Channel: #{remote.tv.channel}"
+puts "Is power on?: #{tv.power}"
+puts "Volume: #{tv.volume}"
+puts "Channel: #{tv.channel}"
 puts "---------------"
 
 puts "TESTING SET CHANNEL (Change to 32):"
 remote.set_channel(32)
-puts "Is power on?: #{remote.tv.power}"
-puts "Volume: #{remote.tv.volume}"
-puts "Channel: #{remote.tv.channel}"
+puts "Is power on?: #{tv.power}"
+puts "Volume: #{tv.volume}"
+puts "Channel: #{tv.channel}"
 puts "---------------"
 
 puts "the end"
